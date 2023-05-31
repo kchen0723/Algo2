fb65279451f231eecc5c6ddee2f287b4517eb115
kissinger chen <kchen@aurorasolartech.com>
2023-05-30 22:33:25 -0700

    add binary search

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int FindInUniqueSortedArray(int[] sortedArray, int target)
+        {
+            if (sortedArray == null || sortedArray.Length == 0)
+            {
+                return -1;
+            }
+
+            var start = 0;
+            var end = sortedArray.Length - 1;
+            while (start <= end)
+            {
+                var middle = start + (end - start) / 2;
+                var current = sortedArray[middle];
+                if (current < target)
+                {
+                    start = middle + 1;
+                }
+                else if (current == target)
+                {
+                    return middle;
+                }
+                else if (current > target)
+                {
+                    end = middle - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
