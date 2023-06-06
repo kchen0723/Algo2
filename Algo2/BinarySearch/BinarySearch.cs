09005b198fe0e6bb7acbd17f63b006c4945fa308
kissinger chen <kchen@aurorasolartech.com>
2023-06-05 21:35:22 -0700

    Init Algo2 solution

---
+++ Algo2/BinarySearch/BinarySearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int FindInSortedUniqueArray(int[] arr, int target)
+        {
+            return -1;
+        }
+    }
+}
---
+++ Algo2Tests/BinarySearch/BinarySearchTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.BinarySearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.BinarySearch.Tests
+{
+    [TestClass()]
+    public class BinarySearchTests
+    {
+        [TestMethod()]
+        public void FindInSortedUniqueArrayTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
