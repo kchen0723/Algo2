3857a89ddeda69ede14bee7fd642cf7590e962de
kissinger <kchen0723@gmail.com>
2023-07-25 21:49:40 -0700

    add Permutation.cs

---
+++ Algo2/Permutation/Permutation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Permutation
+{
+    public class Permutation
+    {
+        public static List<List<int>> GetPermutationFromUniqueArray(int[] nums, int length)
+        { 
+            var result = new List<List<int>>();
+            if (nums == null || nums.Length == 0 || length == 0 || length > nums.Length)
+            {
+                return result;   
+            }
+
+            var selected = new bool[nums.Length];
+            var candidate = new List<int>();
+            GetPermutationFromUniqueArrayHelp(nums, length, selected, candidate, result);
+            return result;
+        }
+
+        private static void GetPermutationFromUniqueArrayHelp(int[] nums, int length, bool[] selected, List<int> candidte, List<List<int>> result) 
+        {
+            if (candidte.Count == length)  //base case
+            { 
+                result.Add(candidte.ToArray().ToList());
+            }
+
+            for(var i = 0; i < nums.Length; i++) 
+            {
+                if (selected[i]) 
+                {
+                    continue;
+                }
+
+                selected[i] = true;
+                candidte.Add(nums[i]);
+                GetPermutationFromUniqueArrayHelp(nums, length, selected, candidte, result);
+                candidte.RemoveAt(candidte.Count - 1);
+                selected[i] = false;
+            }
+        }
+    }
+}
---
+++ Algo2Tests/Permutation/PermutationTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Permutation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Permutation.Tests
+{
+    [TestClass()]
+    public class PermutationTests
+    {
+        [TestMethod()]
+        public void GetPermutationFromUniqueArrayTest()
+        {
+            var nums = new int[] { 1, 2, 3};
+            var permutations = Permutation.GetPermutationFromUniqueArray(nums, 3);
+        }
+    }
+}
