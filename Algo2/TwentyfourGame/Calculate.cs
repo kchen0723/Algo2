c91f385cd360cda8d750700182eed7be2068308e
kissinger <kchen0723@gmail.com>
2023-08-06 12:44:17 -0700

    Add Calculate.cs

---
+++ Algo2/TwentyfourGame/Calculate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Algo2.TwentyfourGame
+{
+    public class Calculate
+    {
+        public static List<string> GetTokens(string src)
+        {
+            var result = new List<string>();
+            if (string.IsNullOrEmpty(src))
+            {
+                return result;
+            }
+
+            var pattern = "[0-9]+[\\.0-9]*|[\\(\\)+\\-\\*/]";
+            var mathResult = Regex.Matches(src, pattern);
+            for (var i = 0; i < mathResult.Count; i++)
+            { 
+                var item = mathResult[i];
+                result.Add(item.Value);
+            }
+            return result;
+        }
+    }
+}
---
+++ Algo2Tests/TwentyfourGame/CalculateTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.TwentyfourGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.TwentyfourGame.Tests
+{
+    [TestClass()]
+    public class CalculateTests
+    {
+        [TestMethod()]
+        public void GetTokensTest()
+        {
+            var src = "11+5.2-23*(0.4+23)";
+            var actual = Calculate.GetTokens(src);
+        }
+    }
+}
