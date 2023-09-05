835ce386b3113f6318440aed1626bde4d1735468
kissinger <kchen0723@gmail.com>
2023-09-05 13:28:18 -0700

    add BinarySearchTree.cs

---
+++ Algo2/Tree/BinarySearchTree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree
+{
+    public class BinarySearchTree<T> where T : IComparable
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public TreeNode<T> Find(T target)
+        {
+            if (Root == null)
+            { 
+                return null;
+            }
+            var current = Root;
+            while (current != null)
+            {
+                var compareResult = current.NodeValue.CompareTo(target);
+                if (compareResult == 0)
+                {
+                    return current;
+                }
+                else if (compareResult < 0)
+                {
+                    current = current.Right;
+                }
+                else if (compareResult > 0)
+                {
+                    current = current.Left;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Algo2Tests/Tree/BinarySearchTreeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree.Tests
+{
+    [TestClass()]
+    public class BinarySearchTreeTests
+    {
+        [TestMethod()]
+        public void FindTest()
+        {
+            TreeNode<int> five = new TreeNode<int>() { NodeValue = 5 };
+            TreeNode<int> three = new TreeNode<int>() { NodeValue = 3 };
+            TreeNode<int> seven = new TreeNode<int>() { NodeValue = 7, Left = three, Right = five };
+
+            TreeNode<int> six = new TreeNode<int>() { NodeValue = 6 };
+            TreeNode<int> eight = new TreeNode<int>() { NodeValue = 8, Left = six };
+
+            TreeNode<int> nine = new TreeNode<int>() { NodeValue = 9, Left = seven, Right = eight };
+
+            BinarySearchTree<int> bt = new BinarySearchTree<int>() { Root = nine };
+            var result = bt.Find(3);
+            Assert.IsNotNull(result);
+        }
+    }
+}
