ab35f08eaee2440c84aedc7b266c03c6c4987166
kissinger <kchen0723@gmail.com>
2023-09-04 22:18:12 -0700

    add BinaryTree.cs

---
+++ Algo2/Tree/BinaryTree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public BinaryTree() 
+        {
+        }
+        public BinaryTree(T data)
+        { 
+            Root = new TreeNode<T>() { NodeValue = data };    
+        }
+
+        public void PreOrder(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.WriteLine(node.NodeValue.ToString());
+            PreOrder(node.Left); 
+            PreOrder(node.Right);
+        }
+
+        //how to do it without recursive?
+        public void InOrder(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            InOrder(node.Left);
+            Console.WriteLine(node.NodeValue.ToString());
+            InOrder(node.Right);
+        }
+
+        public void PostOrder(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            PostOrder(node.Left);
+            PostOrder(node.Right);
+            Console.WriteLine(node.NodeValue.ToString());
+        }
+
+        public void LevelOrder(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+            queue.Enqueue(node);
+            while(queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                Console.WriteLine(current.NodeValue.ToString());
+
+                if (current.Left != null)
+                { 
+                    queue.Enqueue(current.Left);
+                }
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
+        }
+
+        public TreeNode<T> GetLowestCommonAncestor(T n1, T n2)
+        {
+            return GetLowestCommonAncestorHelp(Root, n1, n2);
+        }
+
+        private TreeNode<T> GetLowestCommonAncestorHelp(TreeNode<T> current, T n1, T n2)
+        {
+            if (current == null)
+            {
+                return null;
+            }
+            if (current.NodeValue.CompareTo(n1) == 0)
+            {
+                return current;
+            }
+            if (current.NodeValue.CompareTo(n2) == 0)
+            {
+                return current;
+            }
+
+            var left = GetLowestCommonAncestorHelp(current.Left, n1, n2);
+            var right = GetLowestCommonAncestorHelp(current.Right, n1, n2);
+            if (left != null && right != null)
+            {
+                return current;
+            }
+            if (left != null)
+            {
+                return left;
+            }
+            if (right != null)
+            {
+                return right;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Algo2Tests/Tree/BinaryTreeTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree.Tests
+{
+    [TestClass()]
+    public class BinaryTreeTests
+    {
+        [TestMethod()]
+        public void GetLowestCommonAncestorTest()
+        {
+            TreeNode<int> nine = new TreeNode<int>() { NodeValue = 9 };
+            TreeNode<int> six = new TreeNode<int>() { NodeValue = 6, Left = nine };
+            TreeNode<int> five = new TreeNode<int>() { NodeValue = 5 };
+
+            TreeNode<int> seven = new TreeNode<int>() { NodeValue = 7, Left = six, Right = five };
+            TreeNode<int> three = new TreeNode<int>() { NodeValue = 3 };
+
+            TreeNode<int> eight = new TreeNode<int>() { NodeValue = 8, Left = seven, Right = three };
+
+            BinaryTree<int> bt = new BinaryTree<int>() { Root = eight };
+            var result = bt.GetLowestCommonAncestor(9, 5);
+            Assert.AreEqual(8, result.NodeValue);
+        }
+    }
+}
