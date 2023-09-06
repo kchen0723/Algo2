2797b3d2132370f9b8fd3d87cc47141a43c5b211
kissinger <kchen0723@gmail.com>
2023-09-05 21:00:48 -0700

    Add MoveZeroToEnd.cs

---
+++ Algo2/TwoPoints/MoveZeroToEnd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.TwoPoints
+{
+    public class MoveZeroToEnd
+    {
+        public static void Move(int[] arr)
+        {
+            var index = 0;
+            foreach (var item in arr)
+            {
+                if (item != 0)
+                {
+                    arr[index] = item;
+                    index++;
+                }
+            }
+            for (var j = index; j < arr.Length; j++)
+            {
+                arr[j] = 0;
+            }
+        }
+    }
+}
---
+++ Algo2Tests/TwoPoints/MoveZeroToEndTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.TwoPoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.TwoPoints.Tests
+{
+    [TestClass()]
+    public class MoveZeroToEndTests
+    {
+        [TestMethod()]
+        public void MoveTest()
+        {
+            var arr = new int[] { 1, 10, 0, 2, 8, 3, 0, 0, 6, 4, 0, 5, 7, 0 };
+            MoveZeroToEnd.Move(arr);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
