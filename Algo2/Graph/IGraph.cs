5e4983d176b3d6f066f2fc687dd92969a10277b1
kissinger <kchen0723@gmail.com>
2023-09-08 09:52:56 -0700

    Add IGraph.cs

---
+++ Algo2/Graph/IGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Graph
+{
+    public interface IGraph<T>
+    {
+        int GetNumberOfVertex();
+        int GetNumberOfEdge();
+    }
+}
