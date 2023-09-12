8480a160798701631ae2b414fc45fa2a782f1c01
kissinger <kchen0723@gmail.com>
2023-09-11 19:44:57 -0700

    add ArrayProductExceptItemItself.cs

---
+++ Algo2/DP/ArrayProductExceptItemItself.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class ArrayProductExceptItemItself
+    {
+        public static List<int> GetArrayProductExceptItemItself(int[] arr)
+        {
+            var result = new List<int>();
+            if (arr == null || arr.Length < 2)
+            {
+                return result;
+            }
+
+            result.Add(arr[1]);
+            result.Add(arr[0]);
+            for (var i = 2; i < arr.Length; i++)
+            {
+                var last = result[result.Count - 1];
+                for (var j = 0; j < result.Count; j++)
+                {
+                    result[j] *= arr[i];
+                }
+                result.Add(last * arr[i - 1]);
+            }
+            return result;
+        }
+    }
+}
---
+++ Algo2Tests/DP/ArrayProductExceptItemItselfTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class ArrayProductExceptItemItselfTests
+    {
+        [TestMethod()]
+        public void GetArrayProductExceptItemItselfTest()
+        {
+            var arr = new int[] { 2, 7, 3, 4, 6, 9 };
+            var actual = ArrayProductExceptItemItself.GetArrayProductExceptItemItself(arr);
+            Assert.IsNotNull(actual);
+        }
+    }
+}
