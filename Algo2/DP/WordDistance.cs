30d6322d5cb50f6bc0c478e8abe2916b127eafc8
kissinger <kchen0723@gmail.com>
2023-09-25 14:14:17 -0700

    add WordDistance.cs

---
+++ Algo2/DP/WordDistance.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class WordDistance
+    {
+        public static int GetInsertDeleteDistance(string src, string dst)
+        {
+            if (src == null || dst == null)
+            { 
+                return 0; 
+            }
+
+            var dp = new int[src.Length + 1, dst.Length + 1];  // 1) define each src --> dst distance
+            for (var i = 0; i <= src.Length; i++)
+            {
+                for (var j = 0; j <= dst.Length; j++)
+                {
+                    if (i == 0)
+                    {
+                        dp[i, j] = j;                       // 2)　base case
+                    }
+                    else if (j == 0)
+                    {
+                        dp[i, j] = i;                       // 2)　base case
+                    }
+                    else if (src[i - 1] == dst[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];         // 3) transfer formula
+                    }
+                    else
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]) + 1;         // 3) transfer formula, left is for insert, top is for delete
+                    }
+                }
+            }
+            return dp[src.Length, dst.Length];
+        }
+
+        public static int GetEditDistance(string src, string dst)
+        {
+            if (src == null || dst == null)
+            {
+                return 0;
+            }
+
+            var dp = new int[src.Length + 1, dst.Length + 1];
+            for (var i = 0; i <= src.Length; i++)
+            {
+                for (var j = 0; j <= dst.Length; j++)
+                {
+                    if (i == 0)
+                    {
+                        dp[i, j] = j;
+                    }
+                    else if (j == 0)
+                    {
+                        dp[i, j] = i;
+                    }
+                    else if (src[i - 1] == dst[j - 1])
+                    {
+                        dp[i, j] = dp[i - 1, j - 1];       // 3) transfer formula
+                    }
+                    else
+                    {
+                        var left = dp[i - 1, j];
+                        var top = dp[i, j - 1];
+                        var leftTop = dp[i - 1, j - 1];
+                        dp[i, j] = new int[] { left, top, leftTop }.Min() + 1;   // 3) transfer formula, left is for insert, top is for delete, left top is for edit
+                    }
+                }
+            }
+            return dp[src.Length, dst.Length];
+        }
+    }
+}
---
+++ Algo2Tests/DP/WordDistanceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class WordDistanceTests
+    {
+        [TestMethod()]
+        public void GetInsertDeleteDistanceTest()
+        {
+            var result = WordDistance.GetInsertDeleteDistance("dog", "frog");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod()]
+        public void GetEditDistanceTest()
+        {
+            var result = WordDistance.GetEditDistance("dog", "frog");
+            Assert.AreEqual(2, result);
+        }
+    }
+}
