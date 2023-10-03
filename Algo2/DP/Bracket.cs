35ab434a612406bb741222ba03a8b6d8fe7d6bce
kissinger <kchen0723@gmail.com>
2023-10-02 21:43:18 -0700

    add Bracket.cs

---
+++ Algo2/DP/Bracket.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class Bracket
+    {
+        public static bool IsValid(string brackets)
+        {
+            if (brackets == null || brackets.Length == 0)
+            {
+                return false;   
+            }
+
+            var stack = new Stack<char>();
+            foreach ( char item in brackets )
+            {
+                if (item == '(')
+                {
+                    stack.Push(item);
+                }
+                else if (item == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+
+        public static int GetLongestCount(string brackets)
+        {
+            if (brackets == null || brackets.Length == 0)
+            {
+                return 0;
+            }
+
+            var dp = new int[brackets.Length + 1];  //each item means 
+            /*
+            0123456789
+            ((())()()))))()()
+            00024466810
+             */
+            var stack = new Stack<char>();
+            for(var i = 1; i <= brackets.Length; i++)
+            {
+                var item = brackets[i - 1];
+                if (item == '(')
+                {
+                    stack.Push(item);
+                    dp[i] = dp[i - 1];
+                }
+                else if (item == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                        dp[i] = dp[i - 1] + 2;
+                    }
+                }
+            }
+            return dp.Max();
+        }
+    }
+}
---
+++ Algo2Tests/DP/BracketTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class BracketTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var brackets = "((())()()))))()()";
+            var actual = Bracket.IsValid(brackets);
+            Assert.AreEqual(false, actual);
+        }
+
+        [TestMethod()]
+        public void GetLongestCountTest()
+        {
+            var brackets = "((())()()))(((((())))))";
+            var actual = Bracket.GetLongestCount(brackets);
+            Assert.AreEqual(12, actual);
+        }
+    }
+}
