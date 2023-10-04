f09f087f7828bbaacc5bc83a7771d20823993665
kissinger chen <kchen@aurorasolartech.com>
2023-10-03 22:23:56 -0700

    add TriangleMinPath.cs

---
+++ Algo2/DP/TriangleMinPath.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class TriangleMinPath
+    {
+        public static int GetMinPath(List<List<int>> triangle)
+        {
+            if (triangle == null || triangle.Count == 0)
+            {
+                return 0;
+            }
+
+            var dp = new List<List<int>>(); //each item means the min path to that layer from the bottom.
+            dp.Add(triangle[triangle.Count - 1]);  //base case, add the bottom layer to dp.
+            for (var i = triangle.Count - 2; i >= 0; i--)
+            {
+                var layer = new List<int>();
+                for (var j = 0; j < triangle[i].Count; j++)
+                {
+                    var left = triangle[i][j] + dp[0][j];         //transfer formula
+                    var right = triangle[i][j] + dp[0][j + 1];
+                    layer.Add(Math.Min(left, right));
+                }
+                dp.Insert(0, layer);  
+            }
+
+            return dp[0].Min();
+        }
+    }
+}
---
+++ Algo2Tests/DP/TriangleMinPathTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class TriangleMinPathTests
+    {
+        [TestMethod()]
+        public void GetMinPathTest()
+        {
+            var first = new List<int>() { 2 };
+            var second = new List<int>() { 3, 4 };
+            var third = new List<int>() { 6, 5, 7 };
+            var fouth = new List<int>() { 4, 1, 8, 3 };
+            var triangle = new List<List<int>>() { first, second, third, fouth };
+            var min = TriangleMinPath.GetMinPath(triangle);
+            Assert.AreEqual(11, min);
+        }
+    }
+}
