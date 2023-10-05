bd179e2e9fe372b3f30526f1df966a0c0c8f391f
kissinger <kchen0723@gmail.com>
2023-10-05 09:51:24 -0700

    add comment

--- Algo2/DP/SquareSum.cs
+++ Algo2/DP/SquareSum.cs
@@ -15,17 +15,17 @@ namespace Algo2.DP
                 return 0;
             }
 
-            var dp = new int[num + 1];
+            var dp = new int[num + 1];     //each item means the count for that item
             for(var i = 1; i <= num; i++)
             {
                 var root = (int)Math.Floor(Math.Sqrt(i));
                 if (i == root * root)
                 {
-                    dp[i] = 1;
+                    dp[i] = 1;                //transfer formula
                 }
                 else
                 {
-                    dp[i] = dp[root * root] + dp[i - root * root];
+                    dp[i] = dp[root * root] + dp[i - root * root];     //transfer formula
                 }
             }
             return dp[num];
