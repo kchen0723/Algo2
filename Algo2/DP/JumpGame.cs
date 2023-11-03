b07d5079eec4ee50c3aa9a6a298776f314bda8f4
kissinger <kchen0723@gmail.com>
2023-11-02 18:50:37 -0700

    add JumpGame.cs

---
+++ Algo2/DP/JumpGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class JumpGame
+    {
+        //each num means the max step can jump for this index.
+        //for example, [3, 2, 1, 0, 4], the 2 means max 2 steps can jump from here.
+        //return true if can jump to the last number, otherwise, return false.
+        public static bool CanJump(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            { 
+                return false;
+            }
+            var dp = new bool[nums.Length];      //each item means whether can jump to the last element from here.
+            dp[nums.Length - 1] = true;          //base case, set the last element to true as it already reach the last one.
+            for(var i = nums.Length - 2; i >= 0; i--)   //from back to end
+            {
+                var maxStep = nums[i];
+                for (var j = 1; j <= maxStep; j++)
+                {
+                    if (i + j < nums.Length)
+                    {
+                        if (dp[i + j])
+                        {
+                            dp[i] = true;      //transfor formula
+                            break;
+                        }
+                    }
+                }
+            }
+            return dp[0];
+        }
+    }
+}
---
+++ Algo2Tests/DP/JumpGameTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class JumpGameTests
+    {
+        [TestMethod()]
+        public void CanJumpTest()
+        {
+            var nums = new int[] { 3, 0, 0, 1, 4};
+            var actual = JumpGame.CanJump(nums);
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
