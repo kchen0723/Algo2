e7718951444dbd1b1752c7d6981c784bcf2d698e
kissinger <kchen0723@gmail.com>
2023-12-04 21:20:09 -0800

    add TimePlanner.cs

---
+++ Algo2/Pramp/TimePlanner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp
+{
+    public class TimePlanner
+    {
+        public static int GetMeetingPlanner(int[,] slotsA, int[,] slotsB, int duration)
+        {
+            if (slotsA == null || slotsB == null || slotsA.Length == 0 || slotsB.Length == 0)
+            {
+                return 0;
+            }
+
+            var aIndex = 0;
+            var bIndex = 0;
+            while(aIndex < slotsA.Length && bIndex < slotsB.Length)
+            {
+                var start = Math.Max(slotsA[aIndex, 0], slotsB[bIndex, 0]);
+                var end = Math.Min(slotsA[aIndex, 1], slotsB[bIndex, 1]);
+                if (end - start > duration)
+                {
+                    return start;
+                }
+
+                if (slotsA[aIndex, 0] > slotsB[bIndex, 0])
+                {
+                    bIndex++;
+                }
+                else
+                { 
+                    aIndex++;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ Algo2Tests/Pramp/TimePlannerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Pramp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp.Tests
+{
+    [TestClass()]
+    public class TimePlannerTests
+    {
+        [TestMethod()]
+        public void GetMeetingPlannerTest()
+        {
+            var slotA = new int[,] { { 10, 30 }, { 60, 70 }, { 120, 140 } };
+            var slotB = new int[,] { { 15, 20 }, { 61, 78 }, { 130, 200 } };
+            var result = TimePlanner.GetMeetingPlanner(slotA, slotB, 8);
+            Assert.AreEqual(result, 61);
+        }
+    }
+}
