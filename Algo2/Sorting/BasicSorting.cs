d434c812a5ce37c781b49cee0cc430bcae94b347
kissinger <kchen0723@gmail.com>
2023-12-05 21:18:19 -0800

    Add BasicSorting.cs

---
+++ Algo2/Sorting/BasicSorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Sorting
+{
+    public class BasicSorting
+    {
+        public static int[] BubbleSort(int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return input;
+            }
+
+            for(var i = 0; i < input.Length; i++)
+            {
+                for (var j = i; j < input.Length; j++)
+                {
+                    if (input[j] < input[i])
+                    {
+                        var temp = input[i];
+                        input[i] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+    }
+}
---
+++ Algo2Tests/Sorting/BasicSortingTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Sorting.Tests
+{
+    [TestClass()]
+    public class BasicSortingTests
+    {
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            var input = new int[] { 11, 10, 9, 5, 6, 7};
+            var result = BasicSorting.BubbleSort(input);
+            Assert.AreEqual(5, result[0]);
+        }
+    }
+}
