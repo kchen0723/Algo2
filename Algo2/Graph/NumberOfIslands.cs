834858d42f8db03b064262c1616dce64e4630bed
kissinger <kchen0723@gmail.com>
2024-01-03 13:16:34 -0800

    add NumberOfIslands.cs

---
+++ Algo2/Graph/NumberOfIslands.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Graph
+{
+    public class NumberOfIslands
+    {
+        public static int GetNumberOfIslands(int[,] binaryMatrix)
+        {
+            if (binaryMatrix == null || binaryMatrix.Length == 0)
+            {
+                return 0;
+            }
+
+            int rowCount = binaryMatrix.GetLength(0);
+            int columnCount = binaryMatrix.GetLength(1);
+            bool[,] isVisited = new bool[rowCount, columnCount];
+            var result = 0;
+            for(var i = 0; i < rowCount; i++)
+            {
+                for(var j = 0; j < columnCount; j++)
+                {
+                    if (isVisited[i, j] == false && binaryMatrix[i, j] == 1)
+                    { 
+                        result++;
+                        GetNumberOfIslandsHelp(binaryMatrix, i, j, rowCount, columnCount, isVisited);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static void GetNumberOfIslandsHelp(int[,] binaryMatrix, int row, int col, int rowCount, int columnCount, bool[,] isVisited)
+        {
+            if (row < 0 || col < 0 || row >= rowCount || col >= columnCount)
+            {
+                return;
+            }
+            if (isVisited[row, col] == true || binaryMatrix[row, col] == 0)
+            {
+                return;
+            }
+            isVisited[row, col] = true;
+            GetNumberOfIslandsHelp(binaryMatrix, row, col + 1, rowCount, columnCount, isVisited);
+            GetNumberOfIslandsHelp(binaryMatrix, row, col - 1, rowCount, columnCount, isVisited);
+            GetNumberOfIslandsHelp(binaryMatrix, row + 1, col, rowCount, columnCount, isVisited);
+        }
+    }
+}
---
+++ Algo2Tests/Graph/NumberOfIslandsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Graph.Tests
+{
+    [TestClass()]
+    public class NumberOfIslandsTests
+    {
+        [TestMethod()]
+        public void GetNumberOfIslandsTest()
+        {
+            var binaryMatrix = new int[,] { { 1, 0, 1, 0 }, { 0, 1, 1, 1 }, { 0, 0, 1, 0 } };
+            var result = NumberOfIslands.GetNumberOfIslands(binaryMatrix);
+            Assert.AreEqual(result, 2);
+        }
+    }
+}
