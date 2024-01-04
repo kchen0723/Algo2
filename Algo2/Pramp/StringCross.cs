f9b1c3bab0e84afe7ea4178e2f36194719159536
kissinger <kchen0723@gmail.com>
2024-01-04 11:19:17 -0800

    add StringCross.cs

---
+++ Algo2/Pramp/StringCross.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp
+{
+    public class StringCross
+    {
+        //dig -> dog -> dot -> lot -> bot  BFS
+        public static int GetCrossNumber(string[] arr, string target)
+        {
+            if (target == null || arr == null || arr.Length == 0 || arr.Contains(target) == false)
+            {
+                return 0;
+            }
+
+            int result = int.MaxValue;
+            var visited = new List<string>();
+            GetCrossNumberHelp(arr, target, visited, 0, ref result);
+            return result;
+        }
+
+        private static void GetCrossNumberHelp(string[] arr, string target, List<string> visited, int index, ref int result)
+        {
+            if (arr[index] == target)
+            {
+                result = Math.Min(visited.Count, result);
+                return;
+            }
+
+            for(var i = 0; i < arr.Length; i++)
+            {
+                if (visited.Contains(arr[i]))
+                {
+                    continue;
+                }
+                if (CanTransfer(arr[index], arr[i]))
+                {
+                    visited.Add(arr[index]);
+                    GetCrossNumberHelp(arr, target, visited, i, ref result);
+                    visited.RemoveAt(visited.Count - 1);
+                }
+            }
+        }
+
+        //return true when only one character difference
+        private static bool CanTransfer(string source, string target)
+        {
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target) || source.Length != target.Length)
+            {
+                return false;
+            }
+
+            var diff = 0;
+            for(var i = 0; i < source.Length && diff <= 2; i++)
+            {
+                if (source[i] != target[i])
+                { 
+                    diff++;
+                }
+            }
+
+            return diff == 1;
+        }
+    }
+}
---
+++ Algo2Tests/Pramp/StringCrossTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Pramp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp.Tests
+{
+    [TestClass()]
+    public class StringCrossTests
+    {
+        [TestMethod()]
+        public void GetCrossNumberTest()
+        {
+            var arr = new string[] { "dig", "lot", "dog", "bot", "dot" };
+            var result = StringCross.GetCrossNumber(arr, "bot");
+
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
