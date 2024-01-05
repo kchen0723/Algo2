b67f9302bcfe78804158ad3f9563ff1918a7eaaf
kissinger <kchen0723@gmail.com>
2024-01-05 10:30:39 -0800

    add ReverseWords.cs

---
+++ Algo2/Pramp/ReverseWords.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp
+{
+    public class ReverseWords
+    {
+        public static string Reverse(string words)
+        {
+            if (string.IsNullOrEmpty(words))
+            {
+                return words;
+            }
+
+            var wordsArray = words.ToCharArray();
+            var start = 0;
+            var end = 0;
+            while (end < wordsArray.Length)
+            {
+                if (wordsArray[end] == ' ')
+                { 
+                    ReverseHelp(wordsArray, start, end - 1);
+                    start = end + 1;
+                }
+                end++;
+            }
+            ReverseHelp(wordsArray, start, wordsArray.Length - 1);
+            ReverseHelp(wordsArray, 0, wordsArray.Length - 1);
+            return new string(wordsArray);
+        }
+
+        private static void ReverseHelp(char[] words, int start, int end)
+        {
+            if(start >= 0 && end < words.Length)
+            {
+                while(start < end)
+                {
+                    var temp = words[start];
+                    words[start] = words[end];
+                    words[end] = temp;
+                    start++;
+                    end--;
+                }
+            }
+        }
+    }
+}
---
+++ Algo2Tests/Pramp/ReverseWordsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Pramp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Pramp.Tests
+{
+    [TestClass()]
+    public class ReverseWordsTests
+    {
+        [TestMethod()]
+        public void ReverseTest()
+        {
+            var words = "hello world pao";
+            var result = ReverseWords.Reverse(words);
+            Assert.AreEqual("pao world hello", result);
+        }
+    }
+}
