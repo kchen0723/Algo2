9765c440a4b402b232308397e789b567eccbb046
kissinger chen <kchen@aurorasolartech.com>
2024-01-15 11:04:19 -0800

    add WeightRandom.cs

---
+++ Algo2/labuladong/WeightRandom.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class WeightRandom
+    {
+        public static float GetRandomValue(float[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                return -1;
+            }
+            if (arr.Length == 1)
+            {
+                return arr[0];
+            }
+
+            var sum = arr.Sum();
+            var randomValue = new Random().Next(0, (int)Math.Ceiling(sum));
+            var total = 0f;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                total += arr[i];
+                if (total >= randomValue)
+                {
+                    return arr[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/WeightRandomTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class WeightRandomTests
+    {
+        [TestMethod()]
+        public void GetRandomValueTest()
+        {
+            var arr = new float[] { 1, 2, 3, 4.5f };
+            var actual = WeightRandom.GetRandomValue(arr);
+        }
+    }
+}
