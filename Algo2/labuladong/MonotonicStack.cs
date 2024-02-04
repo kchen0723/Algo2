e7e7a29c5b7516a04579afee9409bf348f578ade
kissinger <kchen0723@gmail.com>
2024-02-03 20:38:29 -0800

    add MonotonicStack.cs

---
+++ Algo2/labuladong/MonotonicStack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class MonotonicStack
+    {
+        public static List<int> GetNextGreaterNumbers(List<int> numbers)
+        {
+            var result = new int[numbers.Count];
+            var stack = new Stack<int>();
+            for(var i = numbers.Count - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0 && stack.Peek() <= numbers[i])
+                { 
+                    stack.Pop();
+                }
+                result[i] = (stack.Count == 0 ? -1 : stack.Peek());
+                stack.Push(numbers[i]);
+            }
+            return result.ToList();
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/MonotonicStackTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class MonotonicStackTests
+    {
+        [TestMethod()]
+        public void GetNextGreaterNumbersTest()
+        {
+            var arr = new List<int>() { 2, 1, 2, 4, 3};
+            var result = MonotonicStack.GetNextGreaterNumbers(arr);
+            Assert.IsNotNull(result);
+        }
+    }
+}
