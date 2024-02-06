0b57a72e3a20b2b45abf7568ff9de09c2b21a2a0
kissinger chen <kchen@aurorasolartech.com>
2024-02-05 21:20:50 -0800

    add MonotonicQueue.cs

---
+++ Algo2/labuladong/MonotonicQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class MonotonicQueue
+    {
+        private Queue<int> queue = new Queue<int>();
+
+        public void Push(int value)
+        { 
+            while(queue.Count > 0 && queue.Peek() <= value)
+            {
+                queue.Dequeue();
+            }
+            queue.Enqueue(value);
+        }
+
+        public int Max()
+        {
+            if (queue.Count == 0)
+            {
+                return -1;
+            }
+            return queue.Peek();
+        }
+
+        public void Pop(int value)
+        {
+            if (queue.Count > 0 && queue.Peek() == value)
+            {
+                queue.Dequeue();
+            }
+        }
+    }
+
+    public class MaxSlidingWindow
+    {
+        public static List<int> GetMaxSliding(List<int> nums, int k)
+        { 
+            List<int> result = new List<int>();
+            MonotonicQueue window = new MonotonicQueue();
+            for(int i = 0; i < nums.Count; i++)
+            {
+                window.Push(nums[i]);
+                if (i >= k - 1)
+                {
+                    result.Add(window.Max());
+                    window.Pop(nums[i - k + 1]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/MaxSlidingWindowTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class MaxSlidingWindowTests
+    {
+        [TestMethod()]
+        public void MaxSldingWindowTest()
+        {
+            var nums = new List<int>() { 1, 3, 3, -3, 5, 3, 6, 7};
+            var result = MaxSlidingWindow.GetMaxSliding(nums, 3);
+            Assert.IsNotNull(result);
+        }
+    }
+}
