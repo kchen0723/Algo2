6235480efb49452a7fe52087a146870c1a21fbdd
kissinger <kchen0723@gmail.com>
2024-02-20 22:17:02 -0800

    add ChessQueen.cs

---
+++ Algo2/labuladong/ChessQueen.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class ChessQueen
+    {
+        private const int MaxQueen = 10;
+        public static List<int[,]> SolveNQueen(int n)
+        {
+            var result = new List<int[,]>();
+            if (n < 1 || n > MaxQueen)
+            {
+                return result;
+            }
+
+            var candidate = new int[n, n];
+            SolveNQueenHelp(result, candidate, 0);
+            return result;
+        }
+
+        private static void SolveNQueenHelp(List<int[,]> result, int[,] candidate, int row)
+        {
+            if (row == candidate.GetLength(0))
+            { 
+                result.Add(ArrayDeepCopy(candidate));
+                return;
+            }
+            for (var col = 0; col < candidate.GetLength(0); col++)
+            {
+                if (IsCandidateValidate(candidate, row, col) == false)
+                {
+                    continue;
+                }
+                candidate[row, col] = 1;
+                SolveNQueenHelp(result, candidate, row + 1);
+                candidate[row, col] = 0;
+            }
+        }
+
+        private static int[,] ArrayDeepCopy(int[,] array)
+        {
+            var result = new int[array.GetLength(0), array.GetLength(1)];
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    result[i, j] = array[i, j];
+                }
+            }
+            return result;
+        }
+
+        private static bool IsCandidateValidate(int[,] candidate, int row, int col)
+        {
+            //need not check row
+            //check col
+            var n = candidate.GetLength(0);
+            for(var i = 0; i < n; i++)
+            {
+                if (candidate[i, col] == 1)
+                {
+                    return false;
+                }
+            }
+            //检查右上方
+            for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            {
+                if (candidate[i, j] == 1)
+                { 
+                    return false; 
+                }
+            }
+            //检查左上方
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (candidate[i, j] == 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/ChessQueenTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class ChessQueenTests
+    {
+        [TestMethod()]
+        public void SolveNQueenTest()
+        {
+            var result = ChessQueen.SolveNQueen(1);
+            Assert.AreEqual(1, result.Count);
+
+            result = ChessQueen.SolveNQueen(2);
+            Assert.AreEqual(0, result.Count);
+
+            result = ChessQueen.SolveNQueen(3);
+            Assert.AreEqual(0, result.Count);
+
+            result = ChessQueen.SolveNQueen(4);
+            Assert.AreEqual(2, result.Count);
+
+            result = ChessQueen.SolveNQueen(5);
+            Assert.AreEqual(10, result.Count);
+
+            result = ChessQueen.SolveNQueen(6);
+            Assert.AreEqual(4, result.Count);
+
+            result = ChessQueen.SolveNQueen(7);
+            Assert.AreEqual(40, result.Count);
+
+            result = ChessQueen.SolveNQueen(8);
+            Assert.AreEqual(92, result.Count);
+        }
+    }
+}
