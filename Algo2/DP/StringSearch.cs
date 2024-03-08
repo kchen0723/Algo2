6789ec5c9a29fe5e08fa80427f44b1f02a04fa33
kissinger <kchen0723@gmail.com>
2024-03-07 22:35:20 -0800

    add StringSearch.cs

---
+++ Algo2/DP/StringSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP
+{
+    public class StringSearch
+    {
+        public static int BruteSearch(string text, string pattern)
+        {
+            if (text == null || pattern == null)
+            {
+                return -1;
+            }
+            if (pattern.Length == 0)
+            {
+                return 0;
+            }
+
+            var i = 0;
+            var j = 0;
+            while(i < text.Length && j < pattern.Length)
+            {
+                if (text[i] == pattern[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i = i - j + 1;
+                    j = 0;
+                }
+            }
+
+            if (j == pattern.Length)
+            {
+                return i - j;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Algo2Tests/DP/StringSearchTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.DP.Tests
+{
+    [TestClass()]
+    public class StringSearchTests
+    {
+        [TestMethod()]
+        public void BruteSearchTest()
+        {
+            var actual = StringSearch.BruteSearch("abcabaabcabac", "abaa");
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
