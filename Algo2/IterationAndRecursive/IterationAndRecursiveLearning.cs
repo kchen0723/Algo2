2030a5f015b442f1b7cbe65b28a2703b3dc91991
kissinger <kchen0723@gmail.com>
2024-07-19 12:08:41 -0700

    add IterationAndRecursive/IterationAndRecursiveLearning.cs

---
+++ Algo2/IterationAndRecursive/IterationAndRecursiveLearning.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.IterationAndRecursive
+{
+    public class IterationAndRecursiveLearning
+    {
+        public static void IterationArray(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public static void RecursiveArray(int[] array)
+        {
+            RecursiveArrayHelp(array, 0);
+        }
+
+        public static void RecursiveArrayHelp(int[] array, int currentInex)
+        {
+            if (currentInex >= 0 && currentInex < array.Length)
+            {
+                Console.WriteLine(array[currentInex]);
+                RecursiveArrayHelp(array, currentInex + 1);
+            }
+        }
+
+        public static void IterationLinkedList(LinkedList<int> linkedList)
+        {
+            var current = linkedList.First;
+            while (current != null)
+            {
+                Console.WriteLine(current.Value);
+                current = current.Next;
+            }
+        }
+
+        public static void RecursiveLinkedList(LinkedList<int> linkedList)
+        {
+            RecursiveLinkedListHelp(linkedList.First);
+        }
+
+        public static void RecursiveLinkedListHelp(LinkedListNode<int> current)
+        {
+            if (current != null)
+            {
+                Console.WriteLine(current.Value);
+                RecursiveLinkedListHelp(current.Next);
+            }
+        }
+    }
+}
---
+++ Algo2Tests/IterationAndRecursive/IterationAndRecursiveLearningTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.IterationAndRecursive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.IterationAndRecursive.Tests
+{
+    [TestClass()]
+    public class IterationAndRecursiveLearningTests
+    {
+        [TestMethod()]
+        public void IterationArrayTest()
+        {
+            var array = new int[] { 4, 5, 7, 8, 9 };
+            IterationAndRecursiveLearning.IterationArray(array);
+            IterationAndRecursiveLearning.RecursiveArray(array);
+        }
+
+        [TestMethod()]
+        public void IterationLinkedListTest()
+        {
+            var LinkedList = new LinkedList<int>(new int[] { 4, 5, 7, 8, 9 });
+            IterationAndRecursiveLearning.IterationLinkedList(LinkedList);
+            IterationAndRecursiveLearning.RecursiveLinkedList(LinkedList);
+        }
+    }
+}
