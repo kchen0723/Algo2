32e895843d5b1d05d4921c8b4ab90350a0b446f3
kissinger <kchen0723@gmail.com>
2025-01-13 12:33:23 -0800

    add calculator recursive

---
+++ Algo2/TwentyfourGame/CalculatorRecusive.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.TwentyfourGame
+{
+    public class CalculatorRecusive
+    {
+        public static float ToFloat(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0f;
+            }
+            
+            var result = 0f;
+            var denominator = 0f;
+            foreach (var item in str)
+            {
+                if ((item >= '0' && item <= '9') || item == '.')
+                {
+                    if (item == '.')
+                    {
+                        denominator = 1f;
+                    }
+                    else
+                    {
+                        if (denominator == 0)
+                        {
+                            result = 10 * result + (item - '0');
+                        }
+                        else
+                        {
+                            denominator *= 10;
+                            result += (item - '0') / denominator;
+                        }
+                    }   
+                }
+                else
+                {
+                    return 0f;
+                }
+            }
+            return result;
+        }
+
+        public static float Calculate(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0f;
+            }
+
+            var expressList = new List<char>(str);
+            return CalculateHelp(expressList);
+        }
+
+        public static float CalculateHelp(List<char> expressList)
+        {
+            Stack<float> stack = new Stack<float>();
+            var tokens = new List<char>();
+            var num = 0f;
+            var sign = '+';
+            while(expressList.Count > 0)
+            {
+                var token = expressList[0];
+                expressList.RemoveAt(0);
+                if ((token >= '0' && token <= '9') || token == '.')
+                {
+                    tokens.Add(token);
+                }
+                if(token == '(' || token == ')' || token == '+' || token == '-' || token == '*' || token == '/'
+                    || expressList.Count == 0)
+                {
+                    if (tokens.Count > 0)
+                    {
+                        num = ToFloat(new string(tokens.ToArray()));
+                        tokens.Clear();
+                    }
+                    if (token == '(')
+                    { 
+                        num = CalculateHelp(expressList);
+                    }
+                    switch (sign)
+                    {
+                        case '+':
+                            stack.Push(num);
+                            break;
+                        case '-':
+                            stack.Push(0 - num);
+                            break;
+                        case '*':
+                            stack.Push(stack.Pop() * num);
+                            break;
+                        case '/':
+                            stack.Push(stack.Pop() / num);
+                            break;
+                    }
+                    if (token == ')')
+                    {
+                        break;
+                    }
+                    sign = token;
+                }
+            }
+            return stack.Sum();
+        }
+    }
+}
---
+++ Algo2Tests/TwentyfourGame/CalculatorRecusiveTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.TwentyfourGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Algo2.TwentyfourGame.Tests
+{
+    [TestClass()]
+    public class CalculatorRecusiveTests
+    {
+        [TestMethod()]
+        public void ToFloatTest()
+        {
+            var result = CalculatorRecusive.ToFloat("987.77");
+            if (Math.Abs(result - 987.77) < 0.001)
+            {
+                Console.WriteLine("success");
+            }
+        }
+
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            string input = "1+((9-3)*4-10)/(6-4)+1";
+            var actual = CalculatorRecusive.Calculate(input);
+            Assert.AreEqual(9, actual);
+
+            input = "1+((3*((9-3)*4-10)/(6-4)+1)+2)/3+5";
+            actual = CalculatorRecusive.Calculate(input);
+            Assert.AreEqual(14, actual);
+        }
+    }
+}
