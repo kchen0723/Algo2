2101e0fb27bfae4f9ac1fab46f4b347b65ee1f98
kissinger <kchen0723@gmail.com>
2025-01-18 11:21:53 -0800

    add CountPrime.cs

---
+++ Algo2/labuladong/CountPrime.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class CountPrime
+    {
+        public static int Count(int number)
+        {
+            if (number < 2)
+            {
+                return 0;
+            }
+
+            bool[] isPrimes = new bool[number + 1];
+            for(var i = 0; i < isPrimes.Length; i++)
+            {
+                isPrimes[i] = true;
+            }
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (isPrimes[i])
+                {
+                    for (int j = i * i; j <= number; j += i)  //i的倍数不是素数
+                    {
+                        isPrimes[j] = false;
+                    }
+                }
+            }
+            int result = 0;
+            for (int i = 2; i <= number; i++)
+            {
+                if (isPrimes[i])
+                { 
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/CountPrimeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class CountPrimeTests
+    {
+        [TestMethod()]
+        public void CountTest()
+        {
+            var actual = CountPrime.Count(20);
+            Assert.AreEqual(8, actual);
+        }
+    }
+}
