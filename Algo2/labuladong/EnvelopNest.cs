9f0de652cb7ec480c37426d474a5c24920564a6d
kissinger <kchen0723@gmail.com>
2025-02-01 14:47:38 -0800

    add EnvelopNest.cs

---
+++ Algo2/labuladong/EnvelopNest.cs
@@ -0,0 +1,42 @@
+﻿using Algo2.DP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class Rectangle
+    { 
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    public class EnvelopNest
+    {
+        public static int MaxNestCount(Rectangle[] rectangles)
+        {
+            if (rectangles == null || rectangles.Length == 0)
+            {
+                return 0;
+            }
+
+            var sorted = rectangles.OrderBy(item => item.Width).ThenBy(item => item.Height).ToArray();
+            //var sorted = rectangles;
+            var dp = new int[sorted.Length];
+            for(var i = 0; i < sorted.Length; i++)
+            {
+                dp[i] = 1;
+                for (var j = 0; j < i; j++)
+                {
+                    if (sorted[i].Width > sorted[j].Width && sorted[i].Height > sorted[j].Height)
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            return dp.Max();
+        }   
+    }
+}
---
+++ Algo2Tests/labuladong/EnvelopNestTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class EnvelopNestTests
+    {
+        [TestMethod()]
+        public void MaxNestCountTest()
+        {
+            var r0 = new Rectangle { Width = 1, Height = 13 };
+            var r1 = new Rectangle { Width = 5, Height = 4 };
+            var r2 = new Rectangle { Width = 6, Height = 4 };
+            var r3 = new Rectangle { Width = 6, Height = 7 };
+            var r4 = new Rectangle { Width = 2, Height = 3 };
+            var r5 = new Rectangle { Width = 3, Height = 10 };
+            var rectangles = new Rectangle[] { r0, r1, r2, r3, r4, r5};
+            var actual = EnvelopNest.MaxNestCount(rectangles);  //{2, 3} => {5, 4} => {6, 7}
+            Assert.AreEqual(3, actual);
+        }
+    }
+}
