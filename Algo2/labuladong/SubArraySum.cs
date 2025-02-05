b9bf15ca8a92893822b05304f11cd419f750516d
kissinger <kchen0723@gmail.com>
2025-02-04 16:40:55 -0800

    add SubArraySum.cs

---
+++ Algo2/labuladong/SubArraySum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong
+{
+    public class SubArraySum
+    {
+        public static int SubArraySumCount(int[] array, int target)
+        {
+            if (array == null || array.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = 0;
+                for (int j = i; j >= 0; j--)
+                { 
+                    sum += array[j];
+                    if (sum == target)
+                    { 
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Algo2Tests/labuladong/SubArraySumTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.labuladong;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.labuladong.Tests
+{
+    [TestClass()]
+    public class SubArraySumTests
+    {
+        [TestMethod()]
+        public void SubArraySumCountTest()
+        {
+            var arr = new int[] {3, 5, 2, -2, 4, 1};
+            var actual = SubArraySum.SubArraySumCount(arr, 8);
+            Assert.AreEqual(2, actual);
+        }
+    }
+}
