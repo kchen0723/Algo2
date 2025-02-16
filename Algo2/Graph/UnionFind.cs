05eb79576ff3af6b43d7c3c373c75d97b55a95ba
kissinger <kchen0723@gmail.com>
2025-02-16 12:32:49 -0800

    add UnionFind.cs

---
+++ Algo2/Graph/UnionFind.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Graph
+{
+    public class UnionFind
+    {
+        private int _count;
+        private int[] _parents;
+        private int[] _size;
+
+        public UnionFind() : this(10)
+        { 
+        }
+
+        public UnionFind(int n)
+        { 
+            this._count = n;
+            _parents = new int[n];
+            _size = new int[n];
+            for(var i = 0; i < n; i++)
+            {
+                _parents[i] = i;
+                _size[i] = 1;
+            }
+        }
+        //将树P和Q连接起来
+        public void Union(int p, int q)
+        {
+            int rootP = FindRoot(p);
+            int rootQ = FindRoot(q);
+            if (rootP == rootQ)
+            {
+                return;
+            }
+
+            if (_size[rootP] > _size[rootQ])
+            {
+                _parents[rootQ] = rootP;
+                _size[rootP] += _size[rootQ];
+            }
+            else
+            {
+                _parents[rootP] = rootQ;
+                _size[rootQ] += _size[rootP];
+            }
+            _count--;
+        }
+
+        private int FindRoot(int x)
+        {
+            while (_parents[x] != x)
+            {
+                x = _parents[x];
+            }
+            return x;
+        }
+
+        private int FindRootAndReducePath(int x)
+        {
+            while (_parents[x] != x)
+            {
+                _parents[x] = _parents[_parents[x]];
+                x = _parents[x];
+            }
+            return x;
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            int rootP = FindRoot(p);
+            int rootQ = FindRoot(q);
+            return rootP == rootQ;
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+    }
+}
---
+++ Algo2Tests/Graph/UnionFindTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Graph.Tests
+{
+    [TestClass()]
+    public class UnionFindTests
+    {
+        //给你⼀个数组 equations ，装着若⼲字符串表⽰的算式。每个算式
+        //equations[i] ⻓度都是 4，⽽且只有这两种情况： a==b 或者 a!=b ，其
+        //中 a,b 可以是任意⼩写字⺟。你写⼀个算法，如果 equations 中所有算
+        //式都不会互相冲突，返回 true，否则返回 false。
+        //⽐如说，输⼊ ["a==b","b!=c","c==a"] ，算法返回 false，因为这三个算式
+        //不可能同时正确
+        [TestMethod()]
+        public void UnionFindTest()
+        {
+            var equations = new string[] { "a==b", "b!=c", "c==a"};
+            var result = EquationPossible(equations);
+            Assert.AreEqual(false, result);
+        }
+
+        private bool EquationPossible(string[] equations)
+        {
+            UnionFind uf = new UnionFind(26);
+            foreach (string equation in equations)
+            {
+                if (equation[1] == '=')
+                {
+                    var p = equation[0];
+                    var q = equation[3]; 
+                    uf.Union(p - 'a', q - 'a');
+                }
+            }
+            foreach (string equation in equations)
+            {
+                if (equation[1] == '!')
+                {
+                    var p = equation[0];
+                    var q = equation[3];
+                    if (uf.IsConnected(p - 'a', q - 'a'))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
