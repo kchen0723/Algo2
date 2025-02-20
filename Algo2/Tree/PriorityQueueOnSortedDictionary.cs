16c60d85a2085d44930d6eee58fc5349a4820d9c
kissinger <kchen0723@gmail.com>
2025-02-19 21:12:28 -0800

    add PriorityQueueOnSortedDictionary.cs

---
+++ Algo2/Tree/PriorityQueueOnSortedDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree
+{
+    public class PriorityQueueOnSortedDictionary<Tkey, TValue>
+    {
+        public SortedDictionary<Tkey, Queue<TValue>> _sortedDictionary = new SortedDictionary<Tkey, Queue<TValue>>();
+
+        public void Enqueue(Tkey priority, TValue value)
+        {
+            if (_sortedDictionary.ContainsKey(priority) == false)
+            {
+                _sortedDictionary[priority] = new Queue<TValue>();
+            }
+            _sortedDictionary[priority].Enqueue(value);
+        }
+
+        private Tkey GetFirst()
+        {
+            if (_sortedDictionary.Count == 0)
+            {
+                throw new InvalidOperationException("priority queue is empty");
+            }
+            var result = default(Tkey);
+            foreach(var kvp in _sortedDictionary)
+            {
+                result = kvp.Key;
+                break;
+            }
+            return result;
+        }
+
+        public TValue Dequeue()
+        {
+            if (_sortedDictionary.Count == 0)
+            {
+                throw new InvalidOperationException("priority queue is empty");
+            }
+
+            var firstKey = GetFirst();
+            var result = _sortedDictionary[firstKey].Dequeue();
+            if (_sortedDictionary[firstKey].Count == 0)
+            {
+                _sortedDictionary.Remove(firstKey);
+            }
+            return result;
+        }
+
+        public TValue Peek()
+        {
+            if (_sortedDictionary.Count == 0)
+            {
+                throw new InvalidOperationException("priority queue is empty");
+            }
+
+            var firstKey = GetFirst();
+            return _sortedDictionary[firstKey].Peek();
+        }
+
+        public bool IsEmpty()
+        { 
+            return _sortedDictionary.Count == 0;
+        }
+    }
+}
---
+++ Algo2Tests/Tree/PriorityQueueOnSortedDictionaryTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo2.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo2.Tree.Tests
+{
+    [TestClass()]
+    public class PriorityQueueOnSortedDictionaryTests
+    {
+        [TestMethod()]
+        public void EnqueueTest()
+        {
+            var pq = new PriorityQueueOnSortedDictionary<int, int>();
+            pq.Enqueue(13, 131);
+            pq.Enqueue(1, 11);
+            pq.Enqueue(-10, -101);
+            pq.Enqueue(4, 41);
+            pq.Enqueue(2, 21);
+            while (pq.IsEmpty() == false)
+            {
+                var item = pq.Dequeue();
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
